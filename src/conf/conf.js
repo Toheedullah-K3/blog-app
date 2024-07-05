f222b9e8b0f6504875a5c3bf207f916f282ce1e2
Toheedullah-K3 <toheedullah002@gmail.com>
2024-07-05 16:01:15 +0500

    first commit

---
+++ src/conf/conf.js
@@ -0,0 +1,12 @@
+// Production Grade Approach to use .env Variables
+
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITTE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITTE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITTE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITTE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITTE_BUCKET_ID),
+}
+
+
+export default conf
